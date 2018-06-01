6b541954460b344868ea65ed432c89dc2363297d
franidn <fran95.idn@gmail.com>
2018-06-01 10:42:19 +0200

    nfc testejat a la raspberry i preparat per juntar

--- nfckeg.py
+++ nfckeg.py
@@ -8,3 +8,15 @@ import notification
 class nfckeg(object):
     """ La tasca serveix per poder controlar un sortidor de cervesa mitjançant targetes nfc"""
     def __init__(self):
+        super(nfckeg, self).__init__()
+        self.sensor_flux = sensors.flowmeter("nom_sensor_flux")
+        self.lector_nfc = sensors.rfid("nom_sensor_nfc")
+        self.notificacio = notifications.telegram("nom_notificacio_telegram")
+
+
+    def setup(self):
+        self.uid = None
+        self.sensor_flux.setup()
+        self.lector_nfc.setup()
+
+    def main_loop(self):
--- sensors/nfc.py
+++ sensors/nfc.py
@@ -1,11 +1,44 @@
-from .main import sensor
+from main import sensor
+import RPi.GPIO as GPIO
+import MFRC522
+import time
+import sys
+
+
 
 class rfid(sensor):
     """ Lector rfid """
     def __init__(self, name):
         super(rfid, self).__init__(name)
 
-    def setup():
+    def setup(self):
+        self.nfcobject = MFRC522.MFRC522()
+        self.estat = "a"
+    def detect_target(self):
 
+            (status,TagType) = self.nfcobject.MFRC522_Request(self.nfcobject.PICC_REQIDL)
+            if status == self.nfcobject.MI_OK:
+                self.estat = "Card detected"
+                print self.estat
+                return self.estat
 
-    def get_data():
+    def get_uid(self):
+            (status,self.uid) = self.nfcobject.MFRC522_Anticoll()
+            if status == self.nfcobject.MI_OK:
+                 print "Card read UID: %s,%s,%s,%s" % (self.uid[0], self.uid[1], self.uid[2], self.uid[3])
+
+if __name__ == "__main__":
+    nfc = rfid("lector")
+    nfc.setup()
+    while True:
+        try:
+            time.sleep(4)
+            nfc.detect_target()
+
+            if nfc.estat == 'Card detected':
+                nfc.get_uid()
+
+        except KeyboardInterrupt:
+            print '\ncaught keyboard interrupt!, bye'
+            GPIO.cleanup()
+            sys.exit()
